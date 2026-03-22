42b6d81a09016dc597d589307214bcfaa1e33ff5
Abraham E. Tavarez <abeincwireless@gmail.com>
2026-03-21 21:14:23 -0400

    Added required deps to file, and created etl docs

---
+++ advanced_data_pipeline/etl_pipeline.md
@@ -0,0 +1,9 @@
+# ETL Documentation and Notes
+
+## Getting started
+
+### 1. Project setup 
+ 1. Create the same folder structure you see here
+ 2. Create a virtual environment
+ 3. Install dependencies using requirement.txt
+ 4. Add the data to the project
---
+++ advanced_data_pipeline/etl_pipeline.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sqlalchemy import create_engine,text
+import mysql.connector as dbconnect
+from mysql.connector import Error
+
+
+"""
+TODO: Project Documentation
+"""
+
+# Check version and document requirements.txt file:
+# print(np.__version__)
+# print(pd.__version__)
